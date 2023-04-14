675027fe70d9c2c7b642ee7f90e896d0911835c7
Lauren Delmar <124837632+lrdelmar@users.noreply.github.com>
2023-04-14 19:06:50 +0100

    Updated main.py
    Included some inline css styling to make the website more responsive

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def higher_lower():
-    return "<h1>Guess a number between 0 and 9</h1>" \
-           "<img width=300px src='https://media.giphy.com/media/ukgcHmvflVWjwE8eU5/giphy.gif'>"
+    return "<div style='width:100%;display:grid'><h1 style='text-align:center'>Guess a number between 0 and 9</h1>" \
+           "<img style='width:30%;margin:auto' src='https://media.giphy.com/media/ukgcHmvflVWjwE8eU5/" \
+           "giphy.gif'></div>"
 
 
 random_number = randint(0, 9)
@@ -16,14 +17,14 @@ random_number = randint(0, 9)
 @app.route('/<int:num>')
 def check_num(num):
     if num == random_number:
-        return "<h1 style='color:green'>You guessed it!</h1>" \
-               "<img src='https://media.giphy.com/media/VIPdgcooFJHtC/giphy.gif'>"
+        return "<div style='width:100%;display:grid'><h1 style='color:green;text-align:center' >You guessed it!</h1>" \
+               "<img style='width:30%;margin:auto' src='https://media.giphy.com/media/VIPdgcooFJHtC/giphy.gif'></div>"
     elif num < random_number:
-        return "<h1 style='color:orange'>Too low, try again</h1>" \
-               "<img src='https://media.giphy.com/media/2DqT8eRoIYElG/giphy.gif'>"
+        return "<div style='width:100%;display:grid'><h1 style='color:orange;text-align:center'>Too low, try again</h1>" \
+               "<img style='width:30%;margin:auto' src='https://media.giphy.com/media/2DqT8eRoIYElG/giphy.gif'></div>"
     elif num > random_number:
-        return "<h1 style='color:red'>Too high, try again</h1>" \
-               "<img src='https://media.giphy.com/media/6uMqzcbWRhoT6/giphy.gif'>"
+        return "<div style='width:100%;display:grid'><h1 style='color:red;text-align:center'>Too high, try again</h1>" \
+               "<img style='width:30%;margin:auto' src='https://media.giphy.com/media/6uMqzcbWRhoT6/giphy.gif'></div>"
 
 
 if __name__ == "__main__":
